a02d586314163e4a26c5dfaf89daaa06571bd825
Alexander Chin <alex.chin.dev@gmail.com>
2021-04-02 14:09:14 -0400

    change homepage url

--- src/App.js
+++ src/App.js
@@ -5,18 +5,38 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          Alexander Chin
+        </h1>
+        <h4>
+          Software Developer
+        </h4>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/alexanderfchin/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Resume
         </a>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/alexanderfchin/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         LinkedIn
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/alexfchin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+
       </header>
     </div>
   );
